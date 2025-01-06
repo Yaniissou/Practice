ffe6572571135036b4a4d3fc1803436e309b9344
Yanissou <yanissoumc@gmail.com>
2025-01-06 19:10:07 +0100

    Duel logic

--- src/main/java/fr/yanissou/practice/Practice.java
+++ src/main/java/fr/yanissou/practice/Practice.java
@@ -1,15 +1,25 @@
 package fr.yanissou.practice;
 
+import fr.yanissou.practice.commands.AcceptCommand;
+import fr.yanissou.practice.commands.DuelCommand;
+import fr.yanissou.practice.listeners.PlayerListeners;
+import fr.yanissou.practice.managers.AskDuelManager;
+import fr.yanissou.practice.managers.DuelManager;
+import me.despical.commandframework.CommandFramework;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Practice extends JavaPlugin {
 
     private static Practice instance;
+    private final AskDuelManager askDuelManager = new AskDuelManager();
+    private final DuelManager duelManager = new DuelManager();
     @Override
     public void onEnable() {
         instance = this;
-        // Plugin startup logic
-
+        CommandFramework framework = new CommandFramework(this);
+        registerCommands(framework);
+        registerListeners();
     }
 
     @Override
@@ -17,7 +27,24 @@ public final class Practice extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+    private void registerListeners(){
+        Bukkit.getPluginManager().registerEvents(new PlayerListeners(), this);
+    }
+
+    private void registerCommands(CommandFramework framework){
+        framework.registerCommands(new AcceptCommand());
+        framework.registerCommands(new DuelCommand());
+    }
+
     public static Practice getInstance() {
         return instance;
+    }
+
+    public AskDuelManager getAskDuelManager() {
+        return askDuelManager;
+    }
+
+    public DuelManager getDuelManager() {
+        return duelManager;
     }
 }
---
+++ src/main/java/fr/yanissou/practice/listeners/PlayerListeners.java
@@ -0,0 +1,37 @@
+package fr.yanissou.practice.listeners;
+
+import fr.yanissou.practice.Practice;
+import fr.yanissou.practice.managers.DuelManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.util.UUID;
+
+public class PlayerListeners implements Listener {
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        event.setDeathMessage(null);
+        DuelManager duelManager = Practice.getInstance().getDuelManager();
+        if (duelManager.hasDuel(event.getEntity().getUniqueId())) {
+            duelManager.removeDuel(event.getEntity().getUniqueId());
+        }
+
+        // Reset inventory of the killer
+        if (event.getEntity().getKiller() != null) {
+            final UUID killerUUID = event.getEntity().getKiller().getUniqueId();
+            final Player killerPlayer = Bukkit.getPlayer(killerUUID);
+            if (killerPlayer != null) {
+                duelManager.resetInventory(killerPlayer);
+            }
+        }
+
+        // Reset inventory of the player who died
+        duelManager.resetInventory(event.getEntity());
+
+
+    }
+}
