3237446a197654e7ab550f462330e83d951861e5
EnjoyFelix <elink.chem@gmail.com>
2025-01-06 19:33:17 +0100

    Sword: Changes the ATTACK_SPEED when a player joins

--- src/main/java/fr/yanissou/practice/listeners/PlayerListeners.java
+++ src/main/java/fr/yanissou/practice/listeners/PlayerListeners.java
@@ -3,13 +3,17 @@ package fr.yanissou.practice.listeners;
 import fr.yanissou.practice.Practice;
 import fr.yanissou.practice.managers.DuelManager;
 import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class PlayerListeners implements Listener {
@@ -42,5 +46,23 @@ public class PlayerListeners implements Listener {
 
 
 
+    }
+
+    /**
+     * Handles the player joining to give him the right attribute modifiers
+     * @param event the event
+     */
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
+        final AttributeInstance attributeInstance = Objects.requireNonNull(player.getAttribute(Attribute.ATTACK_SPEED),
+                        "AttributeInstance of type 'ATTACK_SPEED' shouldn't be null !");
+
+        // does the player already have the attributes ?
+        if (attributeInstance.getBaseValue() == 20)
+            return;
+
+        // set the right value
+        attributeInstance.setBaseValue(20);
     }
 }
