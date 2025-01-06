382d4ee33e67f2b7d31c41ab44dda89d0a1d1752
Yanissou <yanissoumc@gmail.com>
2025-01-06 18:17:16 +0100

    Initial commit

---
+++ src/main/java/fr/yanissou/practice/Practice.java
@@ -0,0 +1,17 @@
+package fr.yanissou.practice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Practice extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
